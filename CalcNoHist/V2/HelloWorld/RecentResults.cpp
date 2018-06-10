329b4e5339e1f6cfdc633582247f851289691c4b
justin Murphey <37391363+TheMurphinatur@users.noreply.github.com>
2018-06-10 11:54:46 -0700

    Add files via upload

---
+++ CalcNoHist/V2/HelloWorld/RecentResults.cpp
@@ -0,0 +1,29 @@
+
+#include <vector>
+#include "stdafx.h"
+#include "RecentResults.h"
+#include "Fraction.h"
+
+RecentResults::RecentResults()
+{
+}
+
+
+RecentResults::~RecentResults()
+{
+}
+
+int RecentResults::addResult(const Fraction & f)
+{
+	return 0;
+}
+
+Fraction RecentResults::operator[](int i)
+{
+	return Fraction();
+}
+
+
+
+
+
---
+++ CalcNoHist/V2/HelloWorld/RecentResults.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <vector>
+#include "stdafx.h"
+#include "Fraction.h"
+class RecentResults
+{
+	
+public:
+	RecentResults();
+	~RecentResults();
+	int addResult(const Fraction &f);
+	Fraction operator[](int i);
+	
+};
+
+
+
+
+
+
+
+
+
+/*  class RecentResults
+{
+public:
+	RecentResults();
+	~RecentResults();
+	int addResult(const Fraction &f);
+	Fraction operator[](int i);
+};  */
+
